21333cc09c92be22c8b44f69720fe1ac3c4e03f9
swapnilmali1609-sy <swapnilmali1613@gmail.com>
2025-10-02 14:17:49 +0530

    Update datatypes.js

--- datatypes.js
+++ datatypes.js
@@ -1,2 +1,18 @@
-"use string" //treat all js code as newer version,
-alert("hello")
+"use strict" // treat all js as newer version
+let name="SWAPNIL" //string
+let age=20 //number
+let isLoggedin=true/false //boolean
+
+//number=>2 to the power 53
+//bigint
+//string= ""
+//boolean=>true or false
+//null standalone value
+//undefined=value is not defined
+//symbol=>unique
+
+//object
+console.log(typeof "SWAPNIl");
+//console.log(typeof 3);
+console.log(typeof age);
+console.log(typeof null);
