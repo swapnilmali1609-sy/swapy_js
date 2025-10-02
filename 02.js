3a0af0c61b55fbe0eb0c5a30d8592c485824f9ab
swapnilmali1609-sy <swapnilmali1613@gmail.com>
2025-10-02 13:02:44 +0530

    Add files via upload

---
+++ 02.js
@@ -0,0 +1,12 @@
+const account_id = 12344314
+let account_email = "gshjcgy"
+let account_password = "hvciy53r23"
+account_city = "SOLAPUR"
+    // account_email="cdhtwfd"
+    // account_password="sdcfeuq652"
+account_state = "PANDHARPUR"
+    /*
+    prefer not to use var,
+    because of issue of block scope i.e.{} & functional scope.
+    */
+console.table([account_id, account_email, account_password, account_city, account_state])
