9f7c01b7b107d58e86f5c846f935375fd884ce4d
swapnilmali1609-sy <swapnilmali1613@gmail.com>
2025-10-02 14:14:34 +0530

    Add files via upload
    conversion done

---
+++ conversion.js
@@ -0,0 +1,33 @@
+// let score="swapnil"
+// //console.log(score);
+// console.log(typeof score);
+
+// let value=Number(score)
+// console.log(value);
+// console.log(typeof value);
+
+// let score=null
+// //console.log(score);
+// console.log(typeof score);
+
+// let value=Number(score)
+// console.log(value);
+// console.log(typeof value);
+
+// let score=undefined
+// //console.log(score);
+// console.log(typeof score);
+
+// let value=Number(score)
+// console.log(value);
+// console.log(typeof value);
+
+/* "33"=>33
+"33abc"=>NaN
+true=>1;false=>0
+*/
+
+let Number = 40
+let stringNumber = String(Number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
